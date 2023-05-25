7a0043aa105b22534be83d7ee5fad04233aa3f17
Mert Ernaz <rnzappdroid@gmail.com>
2023-05-25 23:12:12 +0300

    remove char in string

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace structures
+{
+    
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            int x=0;
+            string s="";
+            try{
+                Console.WriteLine("Sözcük giriniz: ");
+                 s=Console.ReadLine();
+
+            }catch(FormatException e){
+                Console.WriteLine("Lütfen sözcük giriniz !");
+            }
+
+             try{
+                Console.WriteLine("Index giriniz: ");
+                 x=Convert.ToInt32(Console.ReadLine());
+
+            }catch(FormatException e){
+                Console.WriteLine("Lütfen sayı giriniz !");
+                
+            }
+
+
+
+            Fırlat(x,s);
+
+
+        }
+
+        public static void Fırlat(int ind,string s){
+
+            s=s.Remove(ind,1);
+            Console.WriteLine(" Sonuc: "+s);
+        }
+
+    }
+}
